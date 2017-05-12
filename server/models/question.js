d4fbd8c7dc8831796a88e51dafe139e5c3d382d6
LazyElephant <tschmidt@lazyelephantstudio.com>
2017-05-12 12:58:08 -0400

    Change question model to take multiple question types and update server to reflect changes.
    Added a type field to differentiate between multipl-choice, multiple-answer, and short-answer question types.
    
    The question model will now automatically combine the correct/incorrect answers together when saved.

--- server/models/question.js
+++ server/models/question.js
@@ -1,35 +1,29 @@
 "use strict";
 
 const mongoose = require('mongoose');
-// change schema to represent different types of questions
-// answers can have 1 or many
-// choices can have zero or many
-// 0 for short answer question
-// need to add type of question
+
 const questionSchema = new mongoose.Schema({
     topic: { type: String, required: true, index: true },
+    // type values: "multiple-choice", "multiple-answer", "short-answer"
     type: {type: String, required: true},
     question: { type:String, required: true, minLength: 10 },
+    // answer: the correct answer(s) of the question
     answers: [{type: String, required: true}],
+    // choices: all possible choices, including false options if necessary
+    // ignored for short-answer questions
     choices: [String],
 });
 
-// format documents for consumption by react app
-questionSchema.statics.formatAll = (questions) => questions.map( format );
+questionSchema.pre('save', function(next) {
+    if (this.type !== "short-answer") {
+        
+        for( let i = 0; i < this.answers.length; i++ ) {
+            let index = Math.floor( Math.random() * this.choices.length );
+            this.choices.splice( index, 0, this.answers[i]);
+        }
+    }
+    console.log(this.choices);
+    next();
+});
 
-questionSchema.statics.format = format;
-
-// TODO: This will most likely be unneccessary after refactor is finished
-function format(question) {
-    let answer = {text:question.answers[0], correct: true};
-    let answers = question.nonAnswers.map( na => {return {text: na, correct:false};})
-    let index = Math.floor( Math.random() * question.nonAnswers.length );
-    answers.splice(index, 0, answer);
-
-    return {
-        topic: question.topic,
-        question: question.question,
-        answers: answers
-    };
-}
 module.exports = mongoose.model("Question", questionSchema);
--- server/server.js
+++ server/server.js
@@ -5,15 +5,17 @@ const {join} = require( 'path');
 const express = require( 'express');
 const bodyParser = require( 'body-parser');
 const Question = require('./models/question');
+const cors = require('cors');
 require( './config/db');
 
 const app = module.exports = express();
+app.use(cors());
 app.use(express.static( join(__dirname, "public") ));
 app.use(bodyParser.json());
 app.get('/', (req, res) => { res.status(200).sendFile( join(__dirname, 'public', 'index.html'));});
 app.get('/api/questions', (req, res) => { 
-    Question.find({}, {_id:0})
-        .then( (docs) => res.status(200).json( Question.formatAll(docs) ))
+    Question.find({}, {_id:0, __v:0})
+        .then( (docs) => res.status(200).json( docs ))
         .catch( err => res.status(500).json({error: err}))
 });
 
