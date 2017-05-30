bc547287c0e3fbf354256033b3a5d45e03f6f39d
LazyElephant <tschmidt@lazyelephantstudio.com>
2017-05-30 12:32:13 -0400

    Add markdown support for questions

--- client/src/components/QuestionCard.js
+++ client/src/components/QuestionCard.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Card from './Card';
+import marked from 'marked';
 import {MultipleChoice, ShortAnswer} from './Answers';
 
 export default class QuestionCard extends React.Component {
@@ -14,13 +15,14 @@ export default class QuestionCard extends React.Component {
 
 
     render() {
+        let question = marked(`### ${this.props.question.question}`, {sanitized: true});
         return (
             <Card className={this.props.className}>
                 <div className="topic">
                     <p>{this.props.question.topic}</p>
                 </div>
                 <div className="question">
-                    <h3>{this.props.question.question}</h3>
+                    <div dangerouslySetInnerHTML={{ __html:question}}></div>
                     {   
                         this.props.question.type === "short-answer" ?
                             <ShortAnswer {...this.props.question} ref={ref=>this.answerComponent=ref}/> :
