434331dcf470054fa78b0a2172513fcf06a65d89
LazyElephant <tschmidt@lazyelephantstudio.com>
2017-07-05 16:32:32 -0400

    Add material-ui

--- src/index.js
+++ src/index.js
@@ -1,8 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
+import injectTapEventPlugin from 'react-tap-event-plugin';
 import registerServiceWorker from './registerServiceWorker';
 import './index.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+injectTapEventPlugin();
+
+const ThemedApp = () => (
+    <MuiThemeProvider>
+        <App />
+    </MuiThemeProvider>
+);
+
+ReactDOM.render(<ThemedApp />, document.getElementById('root'));
 registerServiceWorker();
