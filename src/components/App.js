3428e00acf7c09f9cd1a79946480b2b31eb7f0fd
LazyElephant <tschmidt@lazyelephantstudio.com>
2017-07-15 16:14:47 -0400

    Change helpers folder to lib

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 
-import {getCards} from '../helpers/data-access';
+import {getCards} from '../lib/data-access';
 import Card from './Card';
 
 export default class App extends Component{
