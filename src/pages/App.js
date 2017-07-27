160fbe3691f0c38022a5ef3bff532ceb03d73e70
Tim Schmidt <tschmidt@lazyelephantstudio.com>
2017-07-27 16:36:27 -0400

    Move App component into pages folder

--- src/pages/App.js
+++ src/pages/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 
 import DB from '../lib/CardDB';
-import Card from './Card';
+import Card from '../components/Card';
 
 export default class App extends Component {
    constructor(props, context) {
