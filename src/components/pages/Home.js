6e320b55a67369348e8d20d65a0f1a0d4de07dc1
LazyElephant <tschmidt@lazyelephantstudio.com>
2017-07-31 18:16:29 -0400

    Move background image to public folder and display with css

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,10 +1,9 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import backgroundImage from '../../images/hero-bg.jpg';
 import './Home.css';
 
 const Home = () => (
-  <div className="hero" style={{backgroundImage: backgroundImage}}>
+  <div className="hero">
     <h1>Welcome to QuizMe</ h1>
     <Link to="/cards" className="cta">Get Started Here</ Link>
     <p className="Author">Photo by Aaron Burden on Unsplash</ p>
