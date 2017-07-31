40c8235233b8165c93320bddaf7240523e89bf23
LazyElephant <tschmidt@lazyelephantstudio.com>
2017-07-31 18:04:54 -0400

    Adjust home page styles and change CTA link

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -6,7 +6,7 @@ import './Home.css';
 const Home = () => (
   <div className="hero" style={{backgroundImage: backgroundImage}}>
     <h1>Welcome to QuizMe</ h1>
-    <Link to="/cards/create" className="cta">Create Some Cards</ Link>
+    <Link to="/cards" className="cta">Get Started Here</ Link>
     <p className="Author">Photo by Aaron Burden on Unsplash</ p>
   </div>
 );
