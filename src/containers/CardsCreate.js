5da9bc7759f5f8fee933427e1d91c8aa12075c49
LazyElephant <tschmidt@lazyelephantstudio.com>
2017-07-31 21:52:48 -0400

    Create initial unstyled markup to Cards and CardsCreate container components

--- src/containers/CardsCreate.js
+++ src/containers/CardsCreate.js
@@ -1,7 +1,27 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const CreateCard = () => (
-  <div>create card</div>
-);
+class CreateCard extends Component {
+  handleSubmit = (event) => {
+    event.preventDefault();
+
+  }
+
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <label htmlFor='text'>
+          <input type='text' name='text' placeholder='Question' />
+        </label>
+        <label htmlFor='answer'>
+          <input type='text' name='answers[]' placeholder='Answer' />
+        </label>
+        <label htmlFor='text'>
+          <input type='text' name='choices[]' placeholder='Wrong Choice' />
+        </label>
+        <button type='submit'>Add</button>
+      </form>
+    );
+  }
+}
 
 export default CreateCard;
